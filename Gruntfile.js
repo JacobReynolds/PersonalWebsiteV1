71502b15cd9a40c446f05b6c4edf0ba8e774a04b
Jake Reynolds <reyno511@umn.edu>
2015-07-26 17:47:45 -0500

    concat css

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,9 @@ module.exports = function(grunt) {
           files: {
 	    'js/minified.js': ['home/routeApp.js', 'home/*.*.js', 'about/*.*.js', 'main/*.*.js', 'projects/*.*.js', 'projects/*.js'],
 	    //Declaring explicit file names because angular.min.js has to be loaded before all other angular files
-	    'js/libraries.js': ['js/imports/jquery-2.1.4.min.js', 'js/imports/angular.min.js', 'js/imports/angular.min.js', 'js/imports/angular-animate.js', 'js/imports/angular-route.min.js', 'js/imports/angular-sanitize.min.js', 'js/imports/angular-touch.min.js']
+	    'js/libraries.js': ['js/imports/jquery-2.1.4.min.js', 'js/imports/angular.min.js', 'js/imports/angular.min.js', 'js/imports/angular-animate.js', 'js/imports/angular-route.min.js', 'js/imports/angular-sanitize.min.js', 'js/imports/angular-touch.min.js'],
+	    'style/minified.css': ['*/*.css']
+
 	  }
         }
       }
